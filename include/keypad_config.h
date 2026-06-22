fdc56376cf463eec3eb6ac452498f5f0e5d18484
Yanne <phamtrounggiang2105@.com>
2026-06-22 12:24:16 +0700

    update 22/6

--- include/keypad_config.h
+++ include/keypad_config.h
@@ -6,10 +6,10 @@
 const byte ROWS = 4; // rows
 const byte COLS = 4; // columns
 
-char keys[ROWS][COLS] = {{'1', '2', '3', 'A'},
-                         {'4', '5', '6', 'B'},
-                         {'7', '8', '9', 'C'},
-                         {'*', '0', '#', 'D'}};
+char keys[ROWS][COLS] = {{'1', '2', '3'},
+                         {'4', '5', '6'},
+                         {'7', '8', '9'},
+                         {'*', '0', '#'}};
 byte rowPins[ROWS] = {R1, R2, R3,
                       R4}; // connect to the row pinouts of the keypad
 byte colPins[COLS] = {C1, C2, C3,
